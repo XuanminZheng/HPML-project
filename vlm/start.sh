ddbe77bb13b62c9482ec7b50ae1692ef2c479e07
XuanminZheng <xz3372@columbia.edu>
2025-12-13 03:42:51 +0000

    add profill

--- vlm/start.sh
+++ vlm/start.sh
@@ -4,7 +4,6 @@ vllm serve openbmb/MiniCPM-V-4 \
     --dtype auto \
     --max-model-len 4096 \
     --trust-remote-code \
-    --gpu_memory_utilization 0.9 \
     --port 8000 > vllm.log 2>&1 &
 
 echo "vLLM 已在后台启动，PID: $!"
